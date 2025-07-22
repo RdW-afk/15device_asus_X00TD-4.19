02042432e7bd0be9f5f3ea73e1655201657ad64b
romeo <rbesar210311@gmail.com>
2025-07-22 23:12:42 +0700

    X00TD: Regenerate
    Signed-off-by: romeo <rbesar210311@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,11 +2,11 @@
 
 # Clone kernel tree
 rm -rf kernel/asus/sdm660
-git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b repart2 kernel/asus/sdm660
+git clone --depth=1 https://github.com/rsuntk/android_kernel_asus_sdm660-4.19 kernel/asus/sdm660
 
 # Clone common tree
 rm -rf device/asus/sdm660-common
-git clone https://github.com/Tiktodz/android_device_asus_sdm660-common-4.19 device/asus/sdm660-common
+git clone https://github.com/rdw-afk/15device_asus_sdm660-common-4.19 -b 16 device/asus/sdm660-common
 
 # Clone vendor tree
 rm -rf vendor/asus
@@ -14,9 +14,8 @@ git clone --single-branch https://github.com/Tiktodz/proprietary_vendor_asus-4.1
 
 # Clone keys
 rm -rf vendor/lineage-priv/keys
-rm -rf vendor/evloution-priv/keys
-mkdir -p vendor/lineage-priv/keys
-git clone https://github.com/electrolaboratory/public-keys vendor/lineage-priv/keys/
+#mkdir -p vendor/lineage-priv/keys
+#git clone https://github.com/electrolaboratory/public-keys vendor/lineage-priv/keys/
 
-export BUILD_USERNAME=queen
+export BUILD_USERNAME=tomtzy
 export TZ=Asia/Jakarta
