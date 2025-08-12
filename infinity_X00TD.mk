dea3d2fd9969cf928699e494dbcb1adcc918ce1f
romeo <rbesar210311@gmail.com>
2025-08-12 02:32:30 +0000

    X00TD: Regenerate
    Signed-off-by: romeo <rbesar210311@gmail.com>

--- infinity_X00TD.mk
+++ infinity_X00TD.mk
@@ -10,15 +10,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common crDroid stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
+INFINITY_MAINTAINER := Tokodepia
 
 # Inherit some common device props
-WITH_GMS := false
+WITH_GAPPS := false
 TARGET_HAS_UDFPS := false
 TARGET_ENABLE_BLUR := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
@@ -28,7 +29,7 @@ TARGET_EXCLUDES_AUDIOFX := true
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := infinity_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
